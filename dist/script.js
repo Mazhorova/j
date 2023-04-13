52a9b71001a565423ac4313b798b70ef5fbdddc6
Mazhorova <mazhorova1987@mail.ru>
2023-04-13 20:34:23 +0300

    first commit

---
+++ dist/script.js
@@ -0,0 +1,15 @@
+/*function sayError() {
+  alert ('«Some error occurred!»')
+}
+sayError()*/
+//Задание2
+/*function showError(messege) {
+  alert(`«Error ${messege} occurred!»`)
+}
+
+showError('white color')
+showError('red color')*/
+//Задание3
+function createHeaders(N) {
+}
+console.log();
---
+++ script.ts
@@ -0,0 +1,23 @@
+/*function sayError() {
+  alert ('«Some error occurred!»')
+}
+sayError()*/
+
+//Задание2
+
+/*function showError(messege) {
+  alert(`«Error ${messege} occurred!»`)
+}
+
+showError('white color')
+showError('red color')*/
+
+//Задание3
+
+// function  createHeaders(N) {
+// }
+// console.log()  
+
+
+
+
